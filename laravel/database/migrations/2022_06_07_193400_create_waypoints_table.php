a35eb3966efb9ccc5eeff31df58d5a49d041f9c8
anne <mocchapi@gmail.com>
2022-06-07 21:48:41 +0200

    waypoint migration

---
+++ laravel/database/migrations/2022_06_07_193400_create_waypoints_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('waypoints', function (Blueprint $table) {
+            $table->id();
+            $table->integer("index");
+            // $table->foreignIdFor(WandelRoute::class); // model nodig
+            $table->point("positie");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('waypoints');
+    }
+};
