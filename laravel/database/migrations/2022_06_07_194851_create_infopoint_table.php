630f7e526fffeca53bc8fc47bf42f26d5ec7df62
anne <mocchapi@gmail.com>
2022-06-07 22:14:02 +0200

    infopoint migration

---
+++ laravel/database/migrations/2022_06_07_194851_create_infopoint_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('infopoint', function (Blueprint $table) {
+            $table->id();
+            $table->integer("index"); // volgorde in verhouding met andere punten. gebruikt voor sorteren
+            // $table->foreignIdFor(WandelRoute::class); // model nodig
+            $table->string("naam", 128)->nullable(); // naam van het punt
+            $table->string("afbeelding",255);
+            $table->point("positie"); // long/las positie
+            $table->text("gedicht");
+            $table->text("info")->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('infopoint');
+    }
+};
