65f23e6d5545a2a3161407f470d3ee687fc412ba
anne <mocchapi@gmail.com>
2022-06-15 11:59:04 +0200

    automatisch importeren van infopoints & wandelroutes in de database

---
+++ laravel/database/seeders/AutoDBSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
+
+use Illuminate\Support\Facades\DB;
+
+class AutoDBSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $index = 0;
+        foreach(Storage::disk('wandelroutes')->directories() as $item) { 
+            $index += 1;
+            echo $index;
+            echo ": ";
+            echo $item;
+
+            DB::table('wandelroutes')->insert([
+                'naam' => $item,
+                'id' => $index,
+            ]);
+
+            foreach (Storage::disk('wandelroutes')->directories($item) as $IP_dir) {
+                $IP_index = explode('$',str_replace($item . '/', '',$IP_dir))[0];
+                $json = json_decode(Storage::disk('wandelroutes')->get($IP_dir . '/data.json'), true);
+
+                DB::table('infopoints')->insert([
+                    'index' => $IP_index,
+                    'wandelroute_id' => $index,
+                    'naam' => $json['titel'],
+                    'gedicht' => $json['gedicht'],
+                    'latitude' => $json['latitude'] ?? 0.0,
+                    'longitude' => $json['longitude'] ?? 0.0,
+                    'afbeelding' => $json['afbeelding']
+                ]);
+            }
+        }
+    }
+}
